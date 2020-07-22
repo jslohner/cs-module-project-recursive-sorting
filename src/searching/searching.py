e0c4111384d63c6a38892a686d964fb66175e0aa
Justin Lohner <lohner4@gmail.com>
2020-07-22 10:27:16 -0600

    wrote agnostic_binary_search function

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -36,5 +36,24 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively
 # or iteratively
 def agnostic_binary_search(arr, target):
-	# Your code here
-	return
+	start = 0
+	end = len(arr) - 1
+	while start <= end:
+		mid = (start + end) // 2
+		if not arr:
+			return -1
+		elif arr[0] < arr[-1]:
+			if target == arr[mid]:
+				return mid
+			elif target < arr[mid]:
+				end = mid - 1
+			elif target > arr[mid]:
+				start = mid + 1
+		elif arr[0] > arr[-1]:
+			if target == arr[mid]:
+				return mid
+			elif target < arr[mid]:
+				start = mid + 1
+			elif target > arr[mid]:
+				end = mid - 1
+	return -1
