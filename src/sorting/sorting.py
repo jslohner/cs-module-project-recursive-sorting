9c559f2f75a3261d42996bbee5dc1938ecd3de9e
Justin Lohner <lohner4@gmail.com>
2020-07-22 12:03:38 -0600

    wrote merge_in_place function

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,6 +2,7 @@
 def merge(arrA, arrB):
 	elements = len(arrA) + len(arrB)
 	merged_arr = [0] * elements
+	# my original solution
 	# loop through each element index
 	for i in range(elements):
 		# check if both arrays have elements
@@ -37,6 +38,26 @@ def merge(arrA, arrB):
 	# return merged array
 	return merged_arr
 
+	# while loop with pointer solution
+	# merged_arr = []
+	# a = 0
+	# b = 0
+	# while (a < len(arrA)) and (b < len(arrB)):
+	# 	if arrA[a] < arrB[b]:
+	# 		merged_arr.append(arrA[a])
+	# 		a += 1
+	# 	else:
+	# 		merged_arr.append(arrB[b])
+	# 		b += 1
+	#
+	# if a < len(arrA):
+	# 	merged_arr.extend(arrA[a:])
+	# elif b < len(arrB):
+	# 	merged_arr.extend(arrB[b:])
+	#
+	# return merged_arr
+
+
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
 	# check if array length is greater than 1
@@ -57,9 +78,26 @@ def merge_sort(arr):
 # In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
-	# Your code here
-	return
+	start2 = mid + 1
+	if (arr[mid] < arr[start2]):
+		return
+	while ((start <= mid) and (start2 <= end)):
+		if (arr[start] <= arr[start2]):
+			start += 1
+		else:
+			value = arr[start2]
+			index = start2
+			while (index != start):
+				arr[index] = arr[index - 1]
+				index -= 1
+			arr[start] = value
+			start += 1
+			mid += 1
+			start2 += 1
 
 def merge_sort_in_place(arr, l, r):
-	# Your code here
-	return
+	if (l < r):
+		m = l + (r - l) // 2
+		merge_sort_in_place(arr, l, m)
+		merge_sort_in_place(arr, (m + 1), r)
+		merge_in_place(arr, l, m, r)
